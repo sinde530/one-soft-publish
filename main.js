db6d4bca15715bcb95bb384193f7570cc50ce5d8
Sinde530 <70146617+sinde530@users.noreply.github.com>
2024-06-09 19:44:47 +0900

    Add Comments

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// react-router-dom 형식처럼 데이터 불러오는 것입니다
+/**
+ * <header/>
+ * {outlet} < index.html or faq or my-profile
+ * <footer/>
+ */
 fetch('components/header.html')
   .then((response) => response.text())
   .then((data) => {
@@ -10,6 +16,7 @@ fetch('components/footer.html')
     document.querySelector('body').insertAdjacentHTML('beforeend', data);
   });
 
+// FAQ페이지 Q&A toggle listener
 document.addEventListener('DOMContentLoaded', function () {
   const faqItems = document.querySelectorAll('.faq-item .question');
 
